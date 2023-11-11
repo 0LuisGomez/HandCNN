b3a829957a506bd2bfe7088075b3e8cdf7a1be1a
0LuisGomez <luisgomez2221@gmail.com>
2023-11-11 14:19:10 -0600

    con una mejor segmentacion de color

--- camera_and_detection/test.py
+++ camera_and_detection/test.py
@@ -1,54 +1,52 @@
 import cv2
 import numpy as np
-import os
-import time  # Import the time module
+
+
+# Trackbar callback function does nothing but required for trackbar
+def nothing(x):
+    pass
 
 
 # Function to preprocess the image
-def preprocess_and_visualize(image, visualize=False):
+def preprocess_and_visualize(image):
     # Convert to YCrCb color space
     ycrcb_img = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
 
-    # Split into 3 channels
-    y, cr, cb = cv2.split(ycrcb_img)
-
-    # Adaptive Thresholding for Y channel
-    binary_y = cv2.adaptiveThreshold(
-        y, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2
+    # Use the inRange function to threshold the YCrCb image
+    lower = np.array(
+        [
+            cv2.getTrackbarPos("Y min", "Trackbars"),
+            cv2.getTrackbarPos("Cr min", "Trackbars"),
+            cv2.getTrackbarPos("Cb min", "Trackbars"),
+        ],
+        dtype="uint8",
     )
-
-    # Thresholding Cr channel
-    _, binary_cr = cv2.threshold(cr, 133, 173, cv2.THRESH_BINARY)
-    # Thresholding Cb channel
-    _, binary_cb = cv2.threshold(cb, 77, 127, cv2.THRESH_BINARY)
-
-    # Morphological operations, consider experimenting with the kernel size and shape
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-    morph_y = cv2.morphologyEx(binary_y, cv2.MORPH_CLOSE, kernel)
-    morph_cr = cv2.morphologyEx(binary_cr, cv2.MORPH_CLOSE, kernel)
-    morph_cb = cv2.morphologyEx(binary_cb, cv2.MORPH_CLOSE, kernel)
-
-    # Combining the masks
-    combined_binary = cv2.bitwise_and(morph_cr, morph_cb)
-    combined_binary = cv2.bitwise_and(combined_binary, morph_y)
-
-    # More morphology on combined mask if needed
-    combined_morph = cv2.morphologyEx(combined_binary, cv2.MORPH_CLOSE, kernel)
+    upper = np.array(
+        [
+            cv2.getTrackbarPos("Y max", "Trackbars"),
+            cv2.getTrackbarPos("Cr max", "Trackbars"),
+            cv2.getTrackbarPos("Cb max", "Trackbars"),
+        ],
+        dtype="uint8",
+    )
+    skinMask = cv2.inRange(ycrcb_img, lower, upper)
 
     # Apply the mask to get the segmented hand
-    hand_segment = cv2.bitwise_and(image, image, mask=combined_morph)
-
+    hand_segment = cv2.bitwise_and(image, image, mask=skinMask)
     return hand_segment
 
 
+# Setup trackbars
+cv2.namedWindow("Trackbars")
+cv2.createTrackbar("Y min", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("Cr min", "Trackbars", 133, 255, nothing)
+cv2.createTrackbar("Cb min", "Trackbars", 77, 255, nothing)
+cv2.createTrackbar("Y max", "Trackbars", 255, 255, nothing)
+cv2.createTrackbar("Cr max", "Trackbars", 173, 255, nothing)
+cv2.createTrackbar("Cb max", "Trackbars", 127, 255, nothing)
+
 # Setup access to the webcam
 cap = cv2.VideoCapture(0)
-
-# Define the paths for saving images and masks
-raw_image_path = "path/to/raw_images"
-mask_path = "path/to/masks"
-os.makedirs(raw_image_path, exist_ok=True)
-os.makedirs(mask_path, exist_ok=True)
 
 try:
     while True:
@@ -58,20 +56,11 @@ try:
             break
 
         # Preprocess the captured frame
-        preprocessed_frame = preprocess_and_visualize(frame, visualize=False)
+        preprocessed_frame = preprocess_and_visualize(frame)
 
         # Display the original frame and the preprocessed frame side by side
         combined_display = np.hstack((frame, preprocessed_frame))
         cv2.imshow("Original and Preprocessed Frames", combined_display)
-
-        # Save the raw image and the preprocessed mask
-        if cv2.waitKey(1) & 0xFF == ord("s"):  # Press 's' to save the images
-            timestamp = int(time.time())
-            img_name = f"raw_{timestamp}.png"
-            mask_name = f"mask_{timestamp}.png"
-
-            cv2.imwrite(os.path.join(raw_image_path, img_name), frame)
-            cv2.imwrite(os.path.join(mask_path, mask_name), preprocessed_frame)
 
         # Break the loop with 'q'
         if cv2.waitKey(1) & 0xFF == ord("q"):
